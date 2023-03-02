cea64e240d3fc3c83a337e3aac8db6be563026e4
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-03-01 21:02:32 -0500

    Implement fetchCoordsByIP function.

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP} = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
 
-  console.log('It worked! Returned IP:' , ip);
-});
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-// console.log(fetchMyIP());
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+
+
+// fetchCoordsByIP('162.245.144.188', (error, coords) => {
+//   if (error) {
+//     console.log(`It didn't work! ${error}`);
+//     return;
+//   }
+//   console.log('It worked! Returned coordinates are: ' , coords);
+
+// });
 
 
 
--- iss.js
+++ iss.js
@@ -14,11 +14,36 @@ const fetchMyIP = function(callback) {
       return;
     }
 
-    const ip = JSON.parse(body).ip;
+    const {ip} = JSON.parse(body);
     callback(null, ip);
 
   });
 };
 
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // console.log(response);
+    // console.log(response.statusCode);
 
-module.exports = {fetchMyIP};
+    const data = JSON.parse(body);
+    console.log(data);
+
+    if (!data.success) {
+      const msg = `Success status was: ${data.success}. Server message says: ${data.message} when fetching coordinates.`;
+      callback((msg), null);
+      return;
+    }
+
+    const {latitude, longitude} = JSON.parse(body);
+
+    callback(null, {latitude, longitude});
+  });
+
+};
+
+
+module.exports = {fetchMyIP, fetchCoordsByIP};
